9ef2232d0ae2632b18b9b652dac865f3dab1a773
denilsondev <denilsoncdc10@gmail.com>
2022-06-28 22:05:47 -0300

    ajuste caminho

---
+++ C# explorando a linguagem/P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto P10 - Calcula Poupanca");
+
+        bool investimento = 1000;
+        float rentabilidade = 0.005;
+
+        bool resultado = investimento + investimento * rentabilidade;
+
+        Console.WriteLine($"resultado eh {resultado}");
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
