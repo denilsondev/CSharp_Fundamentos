248d9df506ea54b4142e49910ec4388fa3d3c510
denilsondev <denilsoncdc10@gmail.com>
2022-06-30 22:33:41 -0300

    namespace e propriedades

--- Orientacao a objeto/bytebank/ContaCorrente.cs
+++ Orientacao a objeto/bytebank/ContaCorrente.cs
@@ -1,11 +1,90 @@
+using bytebank.Titular;
+
 namespace bytebank
 {
-    class ContaCorrente
+    public class ContaCorrente
     {
-        public string titular;
-        public string conta;
-        public int numero_agencia;
-        public string nome_agencia;
-        public double saldo;
+
+        public Cliente Titular { get; set; }
+        public string Nome_Agencia { get; set; }
+        public int Numero_agencia
+        {
+            get;
+
+            set;
+            
+
+        }
+        public string Conta
+        {
+            get;
+            set;
+        }
+
+        private double saldo;
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    saldo = value;
+                }
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+
+       
     }
 }
