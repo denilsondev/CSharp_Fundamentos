b5dc2cb98cbd9b485e153a3b5c6ce7da0dcab950
denilsondev <denilsoncdc10@gmail.com>
2022-06-30 21:25:13 -0300

    comportamentos

--- Orientacao a objeto/bytebank/Program.cs
+++ Orientacao a objeto/bytebank/Program.cs
@@ -1,7 +1,6 @@
 ﻿using bytebank;
 
 Console.WriteLine("Boas Vindas ao seu banco, ByteBank!\n");
-
 ContaCorrente conta1 = new ContaCorrente();
 conta1.titular = "André Silva";
 conta1.conta = "110123-X";
@@ -10,12 +9,15 @@ conta1.nome_agencia = "Agência Central";
 conta1.saldo = 100;
 
 ContaCorrente conta2 = new ContaCorrente();
-conta2.titular = "André Silva";
-conta2.conta = "10123-X";
+conta2.titular = "Amanda Nogueira";
+conta2.conta = "10106-X";
 conta2.numero_agencia = 321;
 conta2.nome_agencia = "Agência Central";
 conta2.saldo = 100;
 
-conta1 = conta2;
-
-Console.WriteLine(conta1==conta2);
+Console.WriteLine("Saldo da Amanda pré-transferência: " + conta2.saldo);
+Console.WriteLine("Saldo do André  pré- Transferência: " + conta1.saldo);
+bool transferencia = conta1.Transferir(50, conta2);
+Console.WriteLine("Transferência realizada com sucesso? " + transferencia);
+Console.WriteLine("Saldo da Amanda pós-transferência: " + conta2.saldo);
+Console.WriteLine("Saldo do André  pós- Transferência: " + conta1.saldo);
